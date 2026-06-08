ff664cc1baafa3d83bce4a3407d28fa7f2aa5bfe
Aaditya-Pratap-Singh <itsaadityasingh10@gmail.com>
2026-06-08 22:35:04 +0530

    Add files via upload

---
+++ pelendrom.c
@@ -0,0 +1,20 @@
+// to check whether the given  3 digit number is pelendrom or not
+#include<stdio.h>
+void main()
+{
+	int n,d1,d2,d3,rev;
+	printf("Enter a three digit number");
+	scanf("%d",&n);
+	d1=n/100;
+	d2=(n/10)%10;
+	d3=n%10;
+	rev=d3*100+d2*10+d1*1;
+	if(rev==n)
+	{
+		printf("The given number is pelendrom");
+	}
+	else
+	{
+		printf("Not a pelendrom");
+	}
+}
