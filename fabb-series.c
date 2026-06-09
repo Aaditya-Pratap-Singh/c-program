3be224dc9b3532a3c2e01f7f8440f6bf7cad27a1
Aaditya-Pratap-Singh <itsaadityasingh10@gmail.com>
2026-06-09 17:42:11 +0530

    Add files via upload

---
+++ fabb-series.c
@@ -0,0 +1,15 @@
+// write a program to print fabb series
+#include<stdio.h>
+void main()
+{
+	int n,i,a=-1,b=1,sum;
+	printf("Enter the ending of the series");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		sum=a+b;
+		printf("%d\t",sum);
+		a=b;
+		b=sum;
+	}
+}
