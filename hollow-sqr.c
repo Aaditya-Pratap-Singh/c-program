9b47b0ddadacd0470ab6a23fec5426669ffc42be
Aaditya-Pratap-Singh <itsaadityasingh10@gmail.com>
2026-06-11 22:55:53 +0530

    Add files via upload

---
+++ hollow-sqr.c
@@ -0,0 +1,20 @@
+ //to print a hollow square
+ #include<stdio.h>
+ void main()
+ {
+ 	int i,j,sp,row;
+ 	printf("Enter the value of row");
+ 	scanf("%d",&row);
+    for(i=1;i<=row;i++)
+    {
+    	for(j=1;j<=row;j++)
+    	{
+    		if(i==1||j==1||i==row||j==row)
+    		printf("*");
+    		else
+    		printf(" ");
+		}
+		printf("\n");
+	}
+ 
+}
