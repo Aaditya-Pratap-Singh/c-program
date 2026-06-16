b8b36e0b967cd0391de4801be882a395e1f626c7
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-06-16 12:18:54 +0530

    Add files via upload

---
+++ vowel-conso.c
@@ -0,0 +1,16 @@
+// to check a charactor is vowel or consonant
+#include<stdio.h>
+void main()
+{
+	char alphabet;
+	printf("Enter an alphabet");
+	scanf("%c",&alphabet);
+	if(alphabet=='a' && alphabet=='e' && alphabet=='i' && alphabet=='o' && alphabet=='u')
+	{
+		printf("An alphabet is a Vowel");
+	}
+	else
+	{
+		printf("An alphabet is a Consonant");
+	}
+}
