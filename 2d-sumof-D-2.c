685605acc5067144dea42c1a45d8b1010761ac3d
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-06-23 20:44:05 +0530

    Add files via upload

---
+++ 2d-sumof-D-2.c
@@ -0,0 +1,31 @@
+//2d array sum of diogonals opposite 
+#include<stdio.h>
+void main()
+{
+	int r,c,i,j,sum=0;
+	printf("enter the size of r and c");
+	scanf("%d%d",&r,&c);
+	int a[r][c];
+	printf("Enter the element of matrix\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+    }
+    printf("Display the elements of matrix\n");
+    for(i=0;i<r;i++)
+    {
+    	for(j=0;j<c;j++)
+    	{
+    		printf("%d\t",a[i][j]);
+    		if(i+j==r-1)
+    		sum+= a[i][j];
+		}
+		printf("\n");
+	}
+	printf("sum of diogonals=%d\n",sum);
+}
+
+
