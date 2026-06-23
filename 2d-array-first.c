973c3af74d504dd502041a03735be943697c2c4d
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-06-23 20:43:42 +0530

    Add files via upload

---
+++ 2d-array-first.c
@@ -0,0 +1,28 @@
+//2d array input and output
+#include<stdio.h>
+void main()
+{
+	int r,c,i,j;
+	printf("enter the size of r and c");
+	scanf("%d%d",&r,&c);
+	int a[r][c];
+	printf("Enter the element of matrix\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+    }
+    printf("Display the elements of matrix\n");
+    for(i=0;i<r;i++)
+    {
+    	for(j=0;j<c;j++)
+    	{
+    		printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
