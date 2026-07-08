65c4002ea55dd9065838bf669e9c08db31486415
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-07-08 21:55:35 +0530

    Fix loop increment syntax in series calculation

--- 1-11-111---.c
+++ 1-11-111---.c
@@ -5,7 +5,7 @@ void main()
 	long long int i,n,series=0;
 	printf("Enter the value of n");
 	scanf("%lld",&n); 
-	for(i=1;i<=n;i++)
+	for(i=1;i<=n;i+)
 	{
 		series=(series*10)+1;
 		printf("%lld\t",series);// \t for space 
