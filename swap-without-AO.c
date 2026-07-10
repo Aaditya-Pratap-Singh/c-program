929138982cc88bdba338ba3e96e40212c15a154c
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-07-10 11:21:13 +0530

    Fix formatting of XOR swap operation

--- swap-without-AO.c
+++ swap-without-AO.c
@@ -7,7 +7,7 @@ void main()
 	scanf("%d%d",&a,&b);
 	printf("value of a before swapping: %d\n",a);
 	printf("value of b before swapping: %d\n",b);
-	a=a^b;
+	a=a^b; 
 	b=a^b;
 	a=a^b;
 	printf("value of a after swapping :%d\n",a);
