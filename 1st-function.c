db43c58609d1a610b19bdaa91486d43e9b9bde45
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-07-13 21:27:57 +0530

    Fix merge conflict in 1st-function.c

--- 1st-function.c
+++ 1st-function.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //to print a message by a function
 #include<stdio.h>
 void show();
@@ -10,16 +10,3 @@ void main()
 {
 	show();
 }
-=======
-//to print a message by a function
-#include<stdio.h> 
-void show();
-void show()
-{
-	printf("HELLO WORLD.... ");
-}
-void main() 
-{
-	show();
-}
->>>>>>> a3b3a9cd7e3d97dc3357f9b679ba91d9f1a3bec6
