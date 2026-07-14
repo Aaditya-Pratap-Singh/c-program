2a017ac8a4516fb7a2609328e859101f734325b0
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-07-14 22:15:57 +0530

    Fix formatting of volume calculation in vol-cubd.c

--- vol-cubd.c
+++ vol-cubd.c
@@ -5,6 +5,6 @@ void main()
 	float l,w,h,volume;
 	printf("enter the value of lenght , width and height");
 	scanf("%f%f%f",&l,&w,&h); 
-	volume = l*w*h;
+	volume = l*w*h; 
 	printf("volume of cuboid= %f",volume); 
 }
