dc1d21b7276e162c329a6f3436b80e3f378476d0
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-07-16 15:34:35 +0530

    Fix formatting issues in add.c

--- add.c
+++ add.c
@@ -3,7 +3,7 @@
 void main()
 {
 	float a,b,sum;
-	printf("enter the value of A and B\n");
+	printf("enter the value of A and B\n"); 
 	scanf("%f%f",&a,&b);//& is address operator
 	sum=a+b;
 	printf("sum is : %f\n",sum);
