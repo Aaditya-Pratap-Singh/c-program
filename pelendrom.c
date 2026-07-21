8c73054e2c1b7df53dc276bc694f77f57e6e37fe
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-07-21 21:25:30 +0530

    Refactor palindrome check in pelendrom.c
    Removed duplicate code and cleaned up the structure.

--- pelendrom.c
+++ pelendrom.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // to check whether the given  3 digit number is pelendrom or not
 #include<stdio.h>
 void main()
@@ -19,26 +18,3 @@ void main()
 		printf("Not a pelendrom");
 	}
 }
-=======
-// to check whether the given  3 digit number is pelendrom or not
-// pelendrom -> 11,22,444,545
-#include<stdio.h>
-void main()
-{
-	int n,d1,d2,d3,rev;
-	printf("Enter a three digit number");
-	scanf("%d",&n);
-	d1=n/100;
-	d2=(n/10)%10;
-	d3=n%10;
-	rev=d3*100+d2*10+d1*1;
-	if(rev==n)
-	{
-		printf("The given number is pelendrom");
-	}
-	else
-	{
-		printf("Not a pelendrom");
-	}
-}
->>>>>>> a3b3a9cd7e3d97dc3357f9b679ba91d9f1a3bec6
