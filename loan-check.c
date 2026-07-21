11cd1374581c9907625ac9c8fe432fb7a6ecbd83
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-07-21 21:04:07 +0530

    Remove duplicate loan eligibility check code

--- loan-check.c
+++ loan-check.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //to check weather the person is eligible for loan or not
 #include<stdio.h>
 void main()
@@ -33,39 +32,3 @@ void main()
 		printf("Eligible for Loan");
 	}
 }
-=======
-//to check weather the person is eligible for loan or not
-#include<stdio.h>
-void main()
-{
-	int age,income,credit_score;
-	printf("enter your age:");
-	scanf("%d",&age);
-	
-	printf("Enter your monthly income: ");
-	scanf("%d",&income);
-	
-	printf("Enter your credit_score:");
-	scanf("%d",&credit_score);
-	
-	if(age<21 || age>65)
-	{
-		printf("Not Eligible\n");
-		printf("age lies between 21 and 65\n");
-	}
-	else if(income<=2500)
-	{
-		printf("Not Eligible\n");
-		printf("Minimum salary should be 2500\n");
-	}
-	else if(credit_score < 650)
-	{
-		printf("Not Eligible\n");
-		printf("Credit score should be more then 650\n");
-	}
-	else
-	{
-		printf("Eligible for Loan");
-	}
-}
->>>>>>> 7547e39c535205c73e3395442a0bb112a0d03976
