8562f20c76e0201195ddf2e0ec7629315cf55b08
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-07-29 20:10:11 +0530

    Fix merge conflict in u-trimatrix.c

--- u-trimatrix.c
+++ u-trimatrix.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //pattern in matrix
 void main()
 {
@@ -15,23 +14,3 @@ void main()
 	printf("\n");
     }
 }
-
-=======
-//pattern in matrix
-void main()
-{
-	int i,j,row,count=0;
-	printf("Enter the value of row :");
-	scanf("%d",&row);
-	for (i=1;i<=row;i++)
-	{
-		for(j=1;j<=i;j++)
-		{ 
-		count++;
-		printf("%d\t",count);
-	    }
-	printf("\n");
-    }
-}
-
->>>>>>> a3b3a9cd7e3d97dc3357f9b679ba91d9f1a3bec6
