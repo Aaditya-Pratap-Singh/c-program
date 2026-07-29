88de3298b020cab1d2f4ba1e253d845d780a9067
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-07-29 20:07:16 +0530

    Fix formatting of printf statement in vol-cubd.c

--- vol-cubd.c
+++ vol-cubd.c
@@ -3,7 +3,7 @@
 void main()
 {
 	float l,w,h,volume;
-	printf("enter the value of lenght , width and height");
+	printf("enter the value of lenght , width and height"); 
 	scanf("%f%f%f",&l,&w,&h); 
 	volume = l*w*h; 
 	printf("volume of cuboid= %f",volume); 
