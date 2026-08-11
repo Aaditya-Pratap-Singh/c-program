93e922e6714984e3f590da214ff24c62b38cc246
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-08-11 20:57:33 +0530

    Refactor eligibility check for driving license
    Removed duplicate code and cleaned up the function.

--- eligible-drv.c
+++ eligible-drv.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //to check a person is eligible for driving license
 #include<stdio.h>
 void main()
@@ -15,21 +14,3 @@ void main()
 			printf("Not eligible");
 	}
 }
-=======
-//to check a person is eligible for driving license
-#include<stdio.h>
-void main()
-{
-	int age;
-	printf("Enter your age :");
-	scanf("%d",&age);
-	switch(age>=18)
-	{
-		case 1 :
-			printf("Your are Eligible");
-			break;
-		case 0 :
-			printf("Not Eligible");
-	}
-}
->>>>>>> a3b3a9cd7e3d97dc3357f9b679ba91d9f1a3bec6
