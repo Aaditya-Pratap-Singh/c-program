89b27db6307c7d193669204d38a4b6b7322d8f39
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-08-12 21:05:11 +0530

    Refactor Fibonacci function for recursion

--- fab-series-recursion.c
+++ fab-series-recursion.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 int fab(int n) {
     if (n == 0)
@@ -21,29 +20,3 @@ int main()
     }
     return 0;
 }
-
-=======
-#include <stdio.h>
-int fab(int n) {
-    if (n == 0)
-        return 0;
-    else if (n == 1)
-        return 1;
-    else
-        return fab(n - 1) + fab(n - 2);  
-}
-
-int main() 
-{
-    int n, i;
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
-
-    printf("Fibonacci series up to %d terms:\n", n);
-    for (i = 0; i < n; i++) {
-        printf("%d\t", fab(i));
-    }
-    return 0;
-}
-
->>>>>>> a3b3a9cd7e3d97dc3357f9b679ba91d9f1a3bec6
