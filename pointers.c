5e62ebd5ef5b6603f3386b5f86d91678c5cd23e0
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-08-13 20:49:34 +0530

    Fix pointer assignment and formatting in pointers.c

--- pointers.c
+++ pointers.c
@@ -9,7 +9,7 @@ void main()
 	printf("Value of p=%u\n",p);
 	printf("Value of a using *p=%d\n",*p);
     printf("address of p=%u\n",&p);
-	q=&p;
+	q=&p; 
 	printf("Value of q=%u\n",q);
 	printf("Value of a using **Qs=%u\n",*q);
 	printf("Value of a using **q=%d\n",**q);
