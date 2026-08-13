ade49ca5248fae6172d89d648ab069c9fa3f2518
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-08-13 20:48:59 +0530

    Fix formatting in peri-rec.c

--- peri-rec.c
+++ peri-rec.c
@@ -3,7 +3,7 @@
 void main()
 {
 	float l,b,perimeter;
-	printf("enter the value of Length and Bredth");
+	printf("enter the value of Length and Bredth"); 
 	scanf("%f%f",&l,&b);
 	perimeter = 2*(l+b);
 	printf("Perimeter of rectangle= %f",perimeter);
