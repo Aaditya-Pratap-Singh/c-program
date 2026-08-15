6ec502cc95a7fe7832c4cebf56afd1ac79004d16
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-08-15 20:40:55 +0530

    Fix vowel detection logic in vowel-conso.c
    Corrected the vowel condition check to properly identify vowels.

--- vowel-conso.c
+++ vowel-conso.c
@@ -7,7 +7,7 @@ void main()
 	scanf("%c",&alphabet);
 	if(alphabet=='a' && alphabet=='e' && alphabet=='i' && alphabet=='o' && alphabet=='u') 
 	{
-		printf("An alphabet is a Vowel");
+		printf("An alphabet is a Vowel"); 
 	}
 	else
 	{
