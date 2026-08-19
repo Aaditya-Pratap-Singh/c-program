0a878446505724234e23f20ef342cff343348fdb
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-08-19 20:10:27 +0530

    Fix formatting of TA calculation in GS-NS.c

--- GS-NS.c
+++ GS-NS.c
@@ -5,7 +5,7 @@ void main()
 	float BS, DA,PF,TA,GS,NS;
 	printf("enter the value of BS");
 	scanf("%f",&BS);
-	TA=(BS*10)/100;
+	TA=(BS*10)/100; 
 	DA=(BS*5)/100; 
 	PF=(BS*7.8)/100;  
 	GS= BS+TA+DA;
